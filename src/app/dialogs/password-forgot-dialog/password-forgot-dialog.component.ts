24ec70664bc4efe7ebee73c163d731130a73a9d7
Lars Gröber <larsgroeber7@gmail.com>
2017-11-03 18:48:33 +0100

    added password revert functionality

--- src/app/dialogs/password-forgot-dialog/password-forgot-dialog.component.ts
+++ src/app/dialogs/password-forgot-dialog/password-forgot-dialog.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MdDialogRef } from '@angular/material';
 import { matchOtherValidator } from '../register-dialog/register-dialog.component';
+import { HttpClient } from '@angular/common/http';
+import { CONFIG } from '../../../config';
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-password-forgot-dialog',
@@ -14,9 +17,13 @@ export class PasswordForgotDialogComponent implements OnInit {
   newPasswordForm: FormGroup;
 
   authenticated = false;
+  readonly authUrl = `${CONFIG.API_URL}/api/v1/password`;
+  readonly resetUrl = `${CONFIG.API_URL}/api/v1/password/reset`;
 
   constructor(public dialogRef: MdDialogRef<PasswordForgotDialogComponent>,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private http: HttpClient,
+              private alert: AlertService) {
     this.createForms();
   }
 
@@ -27,17 +34,38 @@ export class PasswordForgotDialogComponent implements OnInit {
     });
     this.newPasswordForm = this.formBuilder.group({
       token: ['', Validators.required],
-      ppassword: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
       passwordConfirm: ['', [Validators.required, matchOtherValidator('password')]],
     });
   }
 
   authUser() {
-
+    if (this.authForm.invalid) {
+      return;
+    }
+    const name = this.authForm.get('username').value;
+    const email = this.authForm.get('email').value;
+    this.http.post(this.authUrl, {
+      Name: name,
+      Email: email,
+    }).subscribe(() => {
+      this.authenticated = true;
+    }, error => this.alert.showError(error));
   }
 
   resetPassword() {
-
+    if (this.newPasswordForm.invalid) {
+      return;
+    }
+    const token = this.newPasswordForm.get('token').value;
+    const password = this.newPasswordForm.get('password').value;
+    this.http.post(this.resetUrl, {
+      Token: token,
+      NewPassword: password,
+    }).subscribe(() => {
+      this.alert.showSnack('Passwort wurde zurückgesetzt. Bitte neu anmelden.');
+      this.dialogRef.close();
+    }, error => this.alert.showError(error));
   }
 
   ngOnInit() {
